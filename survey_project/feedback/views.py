66ead4db59c06eb85969028670b4f160a9b23fe1
KeneshbekovD2005 <danielkeneshbekov1997@gmail.com>
2025-04-21 23:24:36 +0600

    Описание ваших изменений

--- survey_project/feedback/views.py
+++ survey_project/feedback/views.py
@@ -3,9 +3,13 @@ from .forms import MessageForm
 
 def message_view(request):
     form = MessageForm()
+    success = False  # Добавляем переменную success
+
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
             form.save()
+            success = True  # Устанавливаем success в True при успешной отправке формы
             return redirect('message')
-    return render(request, 'message.html', {'form': form})
+
+    return render(request, 'message.html', {'form': form, 'success': success})  # Передаем success в контекст
